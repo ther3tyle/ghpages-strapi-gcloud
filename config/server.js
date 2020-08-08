0d8d6d7f879f9e19f89b7400712f35e60b20efd6
ther3tyle <shbae0121@gmail.com>
2020-08-08 12:27:24 +0900

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e0315e76ec16c8bc65678a66739469d7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eed6f74a-0c6a-4b89-ba0f-59d26f09909b'
+};
