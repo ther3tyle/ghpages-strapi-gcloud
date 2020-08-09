54007f1ac7f8e0d67906ee605ea9c1b9bad1b21b
ther3tyle <shbae0121@gmail.com>
2020-08-09 18:12:48 +0900

    initial commit

--- config/database.js
+++ config/database.js
@@ -4,12 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+	database: env('DATABASE_NAME'),
+	username: env('DATABASE_USERNAME'),
+	password: env('DATABASE_PASSWORD'),	
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
